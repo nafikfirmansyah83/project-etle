4faaaceec2f045ae41e57b76dc80aa9459b168d1
nafikfirmansyah83 <nafikfirmansyah83@gmail.com>
2024-10-27 08:42:35 +0700

    Changes to be committed: 	new file:   hapus.php

---
+++ hapus.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Memanggil atau membutuhkan file function.php
+require 'function.php';
+
+// Mengambil data dari nis dengan fungsi get
+$kode_transaksi = $_GET['kode_transaksi'];
+
+// Jika fungsi hapus lebih dari 0/data terhapus, maka munculkan alert dibawah
+if (hapus($kode_transaksi) > 0) {
+    echo "<script>
+                alert('Data Pelanggar berhasil dihapus!');
+                document.location.href = 'index.php';
+            </script>";
+} else {
+    // Jika fungsi hapus dibawah dari 0/data tidak terhapus, maka munculkan alert dibawah
+    echo "<script>
+            alert('Data Pelanggar gagal dihapus!');
+        </script>";
+}
